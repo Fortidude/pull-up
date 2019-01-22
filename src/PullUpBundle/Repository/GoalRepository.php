665fd28beb50ba0b05da0a9033a2566531303304
Artur Schroeder <artur@fachowo.co>
2019-01-22 18:02:10 +0100

    fix check duplicate

--- src/PullUpBundle/Repository/GoalRepository.php
+++ src/PullUpBundle/Repository/GoalRepository.php
@@ -211,7 +211,7 @@ class GoalRepository extends AbstractRepository implements GoalRepositoryInterfa
 
     public function checkIfDuplicate(User $user, Exercise $exercise, ExerciseVariant $variant = null)
     {
-        if ((!$exercise || !$exercise->getId()) || (!$variant || !$variant->getId())) {
+        if (!$exercise || !$exercise->getId()) {
             return null;
         }
 
